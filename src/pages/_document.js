fb6cca76cb80ed7e2f7770cf2908afdf8fe6c82c
Sedigh Hamid Reza <H_Sedigh@iranet.net>
2025-08-09 16:27:21 +0330

    'font'

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
         <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png" />
-        <link rel="manifest" href="/site.webmanifest" />
+        {/* <link rel="manifest" href="/site.webmanifest" /> */}
         <meta property="og:image" content="/og-image-1200x630.png" />
         <meta name="theme-color" content="#1E3A8A" />
       </Head>
