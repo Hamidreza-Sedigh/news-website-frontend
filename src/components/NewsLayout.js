53e2c674d3b850501f3c687ccafdf630d135aebc
Hamid Reza Sedigh <hamidr.sedigh@gmail.com>
2025-08-18 22:42:34 +0330

    'icon'

--- src/components/NewsLayout.js
+++ src/components/NewsLayout.js
@@ -15,7 +15,7 @@ export default function NewsLayout({ children, meta }) {
         <meta name="description" content={description?.slice(0, 160)} />
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="icon" href="/favicon.ico" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
 
       </Head>
       <div className="min-h-screen bg-gray-50">
