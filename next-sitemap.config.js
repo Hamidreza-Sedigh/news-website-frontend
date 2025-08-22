6752070d3b72c3cb3b7faed6b06b43f43eb5f448
Hamid Reza Sedigh <hamidr.sedigh@gmail.com>
2025-08-22 13:24:03 +0330

    fix_robots.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: 'http  ://kahrobanet.ir', // دامنه اصلی
-    generateRobotsTxt: true,          // robots.txt هم می‌سازه
-    exclude: ['/admin/*'],            // مسیرهایی که نباید ایندکس بشن
-    sitemapBaseFileName: 'sitemap-static', // اسم فایل میشه sitemap-static.xml
+  siteUrl: 'http://kahrobanet.ir',    // دامنه اصلی سایت
+  generateRobotsTxt: true,             // robots.txt هم ساخته شود
+  exclude: ['/admin/*'],               // مسیرهایی که نباید ایندکس شوند
+  sitemapBaseFileName: 'sitemap-static', // نام فایل اصلی سایت‌مپ
 
-  }
-  
+  // گزینه‌های اضافه برای robots.txt
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'http://kahrobanet.ir/sitemap-news.xml',  // sitemap دوم
+    ],
+  },
+}
