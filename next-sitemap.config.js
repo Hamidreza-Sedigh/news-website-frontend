fee721d33af0ea3bfc85025cf01d673052de2cca
Sedigh Hamid Reza <H_Sedigh@iranet.net>
2025-11-03 12:02:03 +0330

    'exclude-robot'

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   siteUrl: 'http://kahrobanet.ir',    // دامنه اصلی سایت
   generateRobotsTxt: true,             // robots.txt هم ساخته شود
-  exclude: ['/admin/*'],               // مسیرهایی که نباید ایندکس شوند
+  exclude: [
+    '/admin/*',
+    '/dashboard/*', // ← اضافه کن
+    '/login',       // ← اگر صفحه لاگین داری
+    '/register',    // ← اگر صفحه ثبت‌نام داری
+  ],
   sitemapBaseFileName: 'sitemap-static', // نام فایل اصلی سایت‌مپ
 
   // گزینه‌های اضافه برای robots.txt
