877a2f4deb059554ba5641693033304b62440166
Sedigh Hamid Reza <H_Sedigh@iranet.net>
2025-11-29 15:16:02 +0330

    'env'

--- src/pages/api/proxy/news/latest.js
+++ src/pages/api/proxy/news/latest.js
@@ -1,11 +1,19 @@
 //api/proxy/news/latest:
 import { withAuth } from '../middlewares/withAuth';
-import { BACKEND_BASE_URL } from '../config/backend';
+//import { BACKEND_BASE_URL } from '../config/backend';
 
 async function handler(req, res) {
   try {
     const limit = req.query.limit || 30;
-    const response = await fetch(`${BACKEND_BASE_URL}/api/news/latest?limit=${limit}`);
+    const BASE_URL = process.env.BACKEND_URL;
+
+    //const response = await fetch(`${BACKEND_BASE_URL}/api/news/latest?limit=${limit}`);
+    const response = await fetch(`${BASE_URL}/news/latest?limit=${limit}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.BACKEND_ACCESS_TOKEN}`,
+      },
+    });
+    
     const data = await response.json();
     res.status(200).json(data);
   } catch (err) {
--- src/pages/api/proxy/news/popular.js
+++ src/pages/api/proxy/news/popular.js
@@ -1,22 +1,33 @@
+//api/proxy/news/popular:
 import { withAuth } from '../middlewares/withAuth';
-import { BACKEND_BASE_URL } from '../config/backend';
+
 
 async function handler(req, res) {
   try {
     const limit = req.query.limit || 5;
     const period = req.query.period || 'day'; // اضافه شد: day / week / month
+    const BASE_URL = process.env.BACKEND_URL;
+
 
     // درخواست به بک‌اند با limit و period
-    const response = await fetch(`${BACKEND_BASE_URL}/api/news/popular?limit=${limit}&period=${period}`);
-    if (!response.ok) {
-      const text = await response.text();
-      throw new Error(`Backend error: ${text}`);
-    }
-    const data = await response.json();
+    //const response = await fetch(`${BACKEND_BASE_URL}/api/news/popular?limit=${limit}&period=${period}`);
+    const response = await fetch(`${BASE_URL}/news/popular?limit=${limit}&period=${period}`, {
+      headers: {
+        Authorization: `Bearer ${process.env.BACKEND_ACCESS_TOKEN}`,
+      },
+    });
 
-    res.status(200).json(data);
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      console.error(`Backend error: ${errorMessage}`);
+      return res.status(response.status).json({ error: 'Backend error', details: errorMessage });
+    }
+
+    const data = await response.json();
+    return res.status(200).json(data);
+
   } catch (err) {
-    console.error('Error in proxy/popular:', err);
+    console.error('Error in proxy/news/popular:', err);
     res.status(500).json({ error: 'Server error' });
   }
 }
